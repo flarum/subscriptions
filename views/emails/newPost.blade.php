788fb8ade45c6dacebd7ae22534dca1f00aa05f6
variables <lahcenelbouchti@gmail.com>
2018-12-28 13:36:47 +0000

    Update newPost.blade.php

--- views/emails/newPost.blade.php
+++ views/emails/newPost.blade.php
@@ -1,14 +1,14 @@
 Hey {!! $user->display_name !!}!
 
-{!! $blueprint->post->user->display_name !!} made a post in a discussion you're following: {!! $blueprint->post->discussion->title !!}
+{!! $blueprint->post->user->display_name !!} {!! $translator->trans('flarum-subscriptions.forum.notifications.made_new_post') !!}: {!! $blueprint->post->discussion->title !!}
 
 To view the new activity, check out the following link:
 {!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}
 
 ---
 
-{!! $blueprint->post->content !!}
+{!! strip_tags($blueprint->post->contentHtml) !!}
 
 ---
 
-You won't receive any more notifications about this discussion until you're up-to-date.
+{!! $translator->trans('flarum-subscriptions.forum.notifications.receive_notifications') !!}
