420ca518d4dbddeb72ec0121af02c2411e6dec8c
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-28 20:54:51 -0400

    call variables display_name instead of username

--- views/emails/newPost.blade.php
+++ views/emails/newPost.blade.php
@@ -1,6 +1,6 @@
 {!! $translator->trans('flarum-subscriptions.email.new_post.body', [
-'{recipient_username}' => $user->display_name,
-'{poster_username}' => $blueprint->post->user->display_name,
+'{recipient_display_name}' => $user->display_name,
+'{poster_display_name}' => $blueprint->post->user->display_name,
 '{title}' => $blueprint->post->discussion->title,
 '{url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->post->discussion_id, 'near' => $blueprint->post->number]),
 '{content}' => $blueprint->post->content
