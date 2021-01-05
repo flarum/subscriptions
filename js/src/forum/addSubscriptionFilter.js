4fd0eda7309ec23b49957f3d9c903e394b06ebd9
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-05 16:48:50 -0500

    Don't set `params.q` directly
    This avoid accidentially displaying the "clear search" button

--- js/src/forum/addSubscriptionFilter.js
+++ js/src/forum/addSubscriptionFilter.js
@@ -1,6 +1,7 @@
 import { extend } from 'flarum/extend';
 import LinkButton from 'flarum/components/LinkButton';
 import IndexPage from 'flarum/components/IndexPage';
+import DiscussionListState from 'flarum/states/DiscussionListState';
 import GlobalSearchState from 'flarum/states/GlobalSearchState';
 
 export default function addSubscriptionFilter() {
@@ -21,9 +22,15 @@ export default function addSubscriptionFilter() {
     }
   });
 
-  extend(GlobalSearchState.prototype, 'params', function(params) {
-    if (app.current.get('routeName') === 'following') {
-      params.q = (params.q || '') + ' is:following';
+  extend(GlobalSearchState.prototype, 'params', function (params) {
+    // We can't set `q` here directly, as that would make the search bar
+    // think that text has been entered, and display the "clear" button.
+    params.onFollowing = app.current.get('routeName') === 'following';
+  });
+
+  extend(DiscussionListState.prototype, 'requestParams', function (params) {
+    if (this.params.onFollowing) {
+      params.filter.q = (params.filter.q || '') + ' is:following';
     }
   });
 }
